927a49b7eecbc6092ff30f4ed7bd67936ea1c570
Arseniy <arseniy.ustyuzhaninov@yandex.ru>
2023-10-10 00:00:49 +0500

    first lesson

---
+++ README.md
@@ -0,0 +1,9 @@
+# Стек
+ - Laravel: 10
+ - php: 8.1
+ - MySQL: 8.0
+
+# Установка
+ - php artisan migrate
+ - php artisan storage:link
+ - php artisan config:cache
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Events\QueryExecuted;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        /**
+         * Разработка:
+         * preventLazyLoading - Исключение N+1
+         * preventSilentlyDiscardingAttributes - Исключение при заполнении полей модели (fillable)
+         */
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        DB::whenQueryingForLongerThan(500, function (Connection $connection, QueryExecuted $event) {
+            // TODO: 3rd lesson
+        });
+
+        // TODO: 3rd lesson request cycle
+    }
+}
