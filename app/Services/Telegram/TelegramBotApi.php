52756988e858b34cb08c14cc5621cdf0c7d386ec
Arseniy <arseniy.ustyuzhaninov@yandex.ru>
2023-10-13 01:48:24 +0500

    homework-1

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,20 +2,31 @@
 
 namespace App\Services\Telegram;
 
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotApi
 {
     const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chatId, string $text): void
+    public static function sendMessage(string $token, int $chatId, string $text): bool
     {
-        Http::get(
-            self::HOST . $token . '/sendMessage',
-            [
-                'chat_id' => $chatId,
-                'text' => $text,
-            ]
-        );
+        /**
+         * HOMEWORK: обернул в try, с запроса получаю статус код, если успешный (200) отдаю true
+         */
+        try {
+            $result = Http::get(
+                self::HOST . $token . '/sendMessage',
+                [
+                    'chat_id' => $chatId,
+                    'text' => $text,
+                ]
+            );
+
+            return $result->getStatusCode() == 200;
+
+        } catch (ConnectionException $exception) {
+
+        }
     }
 }
