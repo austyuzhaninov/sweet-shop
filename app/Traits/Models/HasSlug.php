a0b9b99f0795bd740e1ef29227d8e6a57690b357
Arseniy <arseniy.ustyuzhaninov@yandex.ru>
2023-11-12 20:51:32 +0500

    slug-generated

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -8,16 +8,73 @@ trait HasSlug
 {
     protected static function bootHasSlug(): void
     {
+        // Добавляем к модели метод создания slug'а
         static::creating(function (Model $item) {
-            $item->slug = $item->slug ??
-                str($item->{self::slugFrom()})
-                    ->append(time()) // TODO: home work
-                    ->slug();
+            $item->makeSlug();
         });
     }
 
-    public static function slugFrom(): string
+    protected function makeSlug(): void
+    {
+        $slug = $this->slugUnique(
+            str($this->{self::slugFrom()})
+                ->slug()
+                ->value()
+        );
+
+        $this->{$this->slugColumn()} = $this->{$this->slugColumn()} ?? $slug;
+    }
+
+    /**
+     * Метот объяевления колонки slug
+     * @return string
+     */
+    protected function slugColumn(): string
+    {
+        return 'slug';
+    }
+
+    /**
+     * Метот объяевления откуда брать строку для генерации slug'га
+     * @return string
+     */
+    protected function slugFrom(): string
     {
         return 'title';
     }
+
+    /**
+     * Генерация уникальных slug'ов
+     * @param string $slug
+     * @return string
+     */
+    private function slugUnique(string $slug): string
+    {
+        $originalSlug = $slug;  //Оригинальный слаг
+        $i = 0;                 // Счетчик для поддержки уникальности
+
+        // Генерация уникального слага, если такой имеется считаем количество слагов и добавляем счетчик в конец (-N)
+        while ($this->isSlugExists($slug)) {
+            $i++;
+            $slug = $originalSlug. '-' .  $i;
+        }
+
+        return $slug;
+    }
+
+    /**
+     * Проверка на существования slag'а в БД текущей модели
+     * @param string $slug
+     * @return bool
+     */
+    private function isSlugExists(string $slug): bool
+    {
+        $query = $this->newQuery()
+            ->where(self::slugColumn(), $slug)                              // Ищем слаг по uid
+            ->where($this->getKeyName(), '!=', $this->getKey())     // Исключаем текущую запись
+            ->withoutGlobalScopes();                                        // Исключаем остальные скопы
+
+        return $query->exists();
+    }
+
 }
